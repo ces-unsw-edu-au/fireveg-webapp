e32a15bd6da1b221ccb44bb2a1bc8422359d332f
JRFP <jr.ferrer.paris@gmail.com>
2022-04-27 13:30:30 +1000

    Show all traits for each species

--- webapp/species.py
+++ webapp/species.py
@@ -11,7 +11,25 @@ from psycopg2.extras import DictCursor
 import pandas as pd
 
 bp = Blueprint('species', __name__, url_prefix='/species')
-create_spp_trait_table="CREATE TEMP TABLE species_traits (species_code,trait_codes) AS (WITH A AS (SELECT 'germ1' AS table_name, species_code FROM litrev.germ1 UNION SELECT 'repr4' AS table_name, species_code FROM litrev.repr4 UNION SELECT 'grow1' AS table_name, species_code FROM litrev.grow1 UNION SELECT 'rect2' AS table_name, species_code FROM litrev.rect2 UNION SELECT 'repr3a' AS table_name, species_code FROM litrev.repr3a UNION SELECT 'repr2' AS table_name, species_code FROM litrev.repr2 UNION SELECT 'surv1' AS table_name, species_code FROM litrev.surv1 UNION SELECT 'repr3' AS table_name, species_code FROM litrev.repr3) SELECT species_code,array_agg(table_name) FROM A GROUP BY species_code)"
+create_spp_trait_table="""
+CREATE TEMP TABLE species_traits (species_code,trait_codes) AS (
+  WITH A AS (
+    SELECT 'repr2' AS table_name, species_code FROM litrev.repr2
+    UNION SELECT 'germ8' AS table_name, species_code FROM litrev.germ8
+    UNION SELECT 'rect2' AS table_name, species_code FROM litrev.rect2
+    UNION SELECT 'germ1' AS table_name, species_code FROM litrev.germ1
+    UNION SELECT 'grow1' AS table_name, species_code FROM litrev.grow1
+    UNION SELECT 'repr4' AS table_name, species_code FROM litrev.repr4
+    UNION SELECT 'surv5' AS table_name, species_code FROM litrev.surv5
+    UNION SELECT 'surv6' AS table_name, species_code FROM litrev.surv6
+    UNION SELECT 'surv7' AS table_name, species_code FROM litrev.surv7
+    UNION SELECT 'disp1' AS table_name, species_code FROM litrev.disp1
+    UNION SELECT 'repr3' AS table_name, species_code FROM litrev.repr3
+    UNION SELECT 'surv4' AS table_name, species_code FROM litrev.surv4
+    UNION SELECT 'surv1' AS table_name, species_code FROM litrev.surv1
+  )
+  SELECT species_code,array_agg(table_name) FROM A GROUP BY species_code
+);"""
 
 @bp.route('/fam_list')
 @login_required
@@ -102,7 +120,7 @@ def sp_info(id):
         samples = None
     traits = list()
 
-    for target in ('surv1','rect2','repr2'):
+    for target in ('surv1','rect2','repr2','disp1','germ8','surv4'):
         qryresp="SELECT species, species_code, norm_value, main_source, count(record_id), sum(weight) as weight FROM litrev.{table} WHERE species_code='{code}' GROUP BY species, species_code, norm_value, main_source;"
         cur.execute(qryresp.format(table=target,code=spp_info[5]))
         if cur.rowcount>0:
@@ -115,7 +133,7 @@ def sp_info(id):
             "metadata":entry.to_dict()
             })
 
-    for target in ('repr3','repr3a','grow1','surv5','surv7'):
+    for target in ('repr3','repr3a','repr4','grow1','surv5','surv6','surv7'):
         qryresp="SELECT record_id, species, species_code, best, lower, upper, main_source FROM litrev.{table} WHERE species_code='{code}';"
         cur.execute(qryresp.format(table=target,code=spp_info[5]))
         if cur.rowcount>0:
